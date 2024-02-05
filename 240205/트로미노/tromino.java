2041aa54ecfde6f73a1a471161fa35092f07c441
oh hyeun taek <79206548+htoh702@users.noreply.github.com>
2024-02-05 19:39:57 +0900

    Title: 트로미노, Time: 169ms, Memory: 12MB, Status: Passed - CodeTree

--- 240205/트로미노/tromino.java
+++ 240205/트로미노/tromino.java
@@ -4,17 +4,15 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    static int[][] list;
-    static int max_count, m, n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        list = new int[n][m];
-        max_count = 0;
+        int[][] list = new int[n][m];
+        int max_count = 0;
 
         for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
@@ -23,31 +21,30 @@ public class Main {
             }
         }
 
+        int total, a=0, b=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<m-1;j++){
+                total = list[i][j] + list[i+1][j] + list[i][j+1] + list[i+1][j+1];
+                int min_list = Math.min(list[i][j], Math.min(list[i+1][j], Math.min(list[i][j+1], list[i+1][j+1])));
+                if(max_count < total - min_list){
+                    max_count = total - min_list;
+                }
+            }
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                find(j, i, 1, 0);
+                if(i+2 < n){
+                    a = list[i][j] + list[i+1][j] + list[i+2][j];
+                }
+                if(j+2 < m){
+                    b = list[i][j] + list[i][j+1] + list[i][j+2];
+                }
+                total = Math.max(a,b);
+                if(max_count < total){
+                    max_count = total;
+                }
             }
         }
         System.out.print(max_count);
-    }
-    public static void find(int x, int y, int index, int sum){
-        sum += list[y][x];
-        if(index >= 3){
-            if(max_count < sum){
-                max_count = sum;
-            }
-            return;
-        }
-
-        if(x+1 < m){
-            find(x+1, y, index+1, sum);
-        }
-        if(x-1 >= 0){
-            find(x-1, y, index+1, sum);
-        }
-        if(y+1 < n){
-            find(x, y+1, index+1, sum);
-        }
-
     }
 }
